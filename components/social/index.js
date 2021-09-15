31aaf8a60a1de1f6f83cebeb8fa9c35c3d112034
Top-Dev-Ops <benjamin.minato.geek@gmail.com>
2021-09-15 18:30:02 +0800

    completed frontend.

--- components/social/index.js
+++ components/social/index.js
@@ -3,22 +3,22 @@ import { Boat, Discord, Instagram, Magnet, Twitter, Youtube } from '../custom/sv
 const Social = ({ extraClassNames }) => {
     return (
         <section className={`social ${extraClassNames}`}>
-            <a href="https://www.twitter.com" target="_blank">
+            <a href="https://www.twitter.com" target="_blank" rel="noreferrer">
                 <Twitter />
             </a>
-            <a href="https://www.discord.com" target="_blank">
+            <a href="https://www.discord.com" target="_blank" rel="noreferrer">
                 <Discord />
             </a>
-            <a href="https://www.instagram.com" target="_blank">
+            <a href="https://www.instagram.com" target="_blank" rel="noreferrer">
                 <Instagram />
             </a>
-            <a href="https://www.youtube.com" target="_blank">
+            <a href="https://www.youtube.com" target="_blank" rel="noreferrer">
                 <Youtube />
             </a>
-            <a href="https://www.google.com" target="_blank">
+            <a href="https://www.google.com" target="_blank" rel="noreferrer">
                 <Magnet />
             </a>
-            <a href="https://www.google.com" target="_blank">
+            <a href="https://www.google.com" target="_blank" rel="noreferrer">
                 <Boat />
             </a>
         </section>
